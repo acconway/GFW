e54a1d2136ffeae8583d911c9a80e3af48622afd
Alexander Conway <alexanderconway@Alexanders-MacBook-Air.local>
2014-12-03 14:49:56 -0800

    Connectors, selection

--- data.js
+++ data.js
@@ -124,6 +124,8 @@ var storyData = {
 	}
 };
 
+var countryData = {}; 
+
 var countryColoring = [2, 3, 4, 2, 2, 0, 0, 0, 0, 1, 2, 2, 2, 2, 1, 1, 3, 0, 3, 3, 2, 1, 0, 1, 0, 3, 3, 1, 2, 0, 1, 0, 2, 0, 1, 1, 1, 0, 1, 0, 3, 0, 3, 1, 1, 1, 0, 1, 1, 0, 1, 2, 2, 0, 0, 1, 0, 1, 1, 2, 1, 1, 2, 1, 1, 0, 0, 1, 1, 0, 0, 2, 1, 2, 0, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 3, 0, 3, 2, 2, 2, 2, 2, 0, 0, 1, 3, 2, 2, 2, 0, 1, 0, 2, 0, 2, 2, 0, 3, 3, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 2, 1, 3, 1, 0, 0, 0, 3, 0, 1, 0, 1, 0, 0, 0, 2, 0, 1, 1, 2, 0, 3, 0, 1, 1, 1, 0, 1, 3, 0, 0, 0, 0, 2, 0, 1, 2, 1, 1, 0, 1, 2, 0, 0, 2, 2, 2, 1];
 
 var cleanCountryData = function(countries) {
@@ -164,7 +166,7 @@ function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
 function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
 
 var hexToRGB = function(hex){
-	return [hexToR(hex),hexToB(hex),hexToG(hex)];
+	return [hexToR(hex),hexToG(hex),hexToB(hex)];
 };
 
 _.each(storyData,function(story){
